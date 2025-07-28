1fa95cfa7054832777278c33820509f07cd7efdf
Ammad Fareed <khalidamphersoft@gmail.com>
2025-07-28 17:53:47 +0500

    remove files

--- C-language/index.c
+++ C-language/index.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
     int length, width, area;
-    length = 70;
-    width = 110;
-    printf("please enter the length of rectangle :\t %d \n", length);
-    // scanf("%f", &length);
-    printf("please enter the width of rectangle :\t %d \n", width);
-    // scanf("%f", &width);
+    printf("please enter the length of rectangle :\t");
+    scanf("%d", &length);
+    printf("please enter the width of rectangle :\t");
+    scanf("%d", &width);
     area = length * width;
-    printf("area of rectangle is :\t %d", area);
-    // i don't know how to get data from user so that's why i comment scanf 's and initialize length width
+    printf("area of rectangle is :\t %d\n", area);
+    return 0;
 }
--- index.c/index.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main ()
-{
-    int age = 20;
-    printf("my age is %i",age);
-    getch();
-}
