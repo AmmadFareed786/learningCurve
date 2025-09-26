14844697a40a80f67b92a2b21b84cbcb4a9b7745
Ammad Fareed <khalidamphersoft@gmail.com>
2025-09-26 12:22:25 +0500

    solve these problems and learning js projects

---
+++ Projects_of_JavaScript_CSS_HTML.html/Console_programs/Q1.js
@@ -0,0 +1,6 @@
+let name = String("Mehmood")
+let age = Number(16)
+console.log(" My name is " + name + " and I am " , age , "years old ") // both + or , use to join 
+
+//Q1: Write a JavaScript program that takes your name and age as variables and prints a sentence like:
+//"My name is ____ and I am ___ years old." in the console. in which fill your name and age according to statement
